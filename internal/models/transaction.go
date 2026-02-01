55f610a0fc36ea7831973b6096de39d6774fb976
Sutthiporn Srisawad <sutthiporn.s2540@gmail.com>
2026-02-01 18:59:30 +0700

    feat: add Transaction model with its types and fields for managing financial transactions.

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -24,7 +24,7 @@ type Transaction struct {
 	Category    *Category       `gorm:"foreignKey:CategoryID" json:"category"`
 	Description string          `json:"description"`
 	ImageUrl    string          `json:"imageUrl"`
-	Date        DateOnly        `gorm:"type:date" json:"date"`
+	Date        time.Time       `json:"date"`
 	CreatedByID string          `gorm:"index" json:"createdById"`
 	CreatedBy   User            `gorm:"foreignKey:CreatedByID" json:"createdBy"`
 	CreatedAt   time.Time       `json:"createdAt"`
